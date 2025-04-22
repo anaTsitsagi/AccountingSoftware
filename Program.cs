f38c1a15c0821996b5f569cbf476d46d6ddbc06b
Ana Tsitsagi <ana.cicagi@gmail.com>
2025-04-22 09:11:15 +0200

    add resource

--- Program.cs
+++ Program.cs
@@ -5,12 +5,24 @@ using System.Globalization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting; // or Microsoft.AspNetCore.Hosting for Server
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+// look for saved culture
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var result = await js.InvokeAsync<string>("localStorage.getItem", "blazorCulture");
+if (!string.IsNullOrEmpty(result))
+{
+    var culture = new CultureInfo(result);
+    CultureInfo.DefaultThreadCurrentCulture = culture;
+    CultureInfo.DefaultThreadCurrentUICulture = culture;
+}
+await host.RunAsync();
