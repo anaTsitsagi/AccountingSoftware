5c0f697753926208f003f98824843ba1167026ea
Ana Tsitsagi <ana.cicagi@gmail.com>
2025-04-28 16:20:44 +0200

    added resources

--- Program.cs
+++ Program.cs
@@ -1,28 +1,57 @@
+using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
 using AccountingSoftware;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Globalization;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting; // or Microsoft.AspNetCore.Hosting for Server
-using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 using Microsoft.JSInterop;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-var host = builder.Build();
-// look for saved culture
-var js = host.Services.GetRequiredService<IJSRuntime>();
-var result = await js.InvokeAsync<string>("localStorage.getItem", "blazorCulture");
-if (!string.IsNullOrEmpty(result))
+namespace AccountingSoftware
 {
-    var culture = new CultureInfo(result);
-    CultureInfo.DefaultThreadCurrentCulture = culture;
-    CultureInfo.DefaultThreadCurrentUICulture = culture;
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            // Root components
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            // HttpClient
+            builder.Services.AddScoped(sp =>
+                new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // 1) Register localization and point at Resources folder
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            var host = builder.Build();
+
+            // 2) Determine culture (from localStorage or fallback to browser)
+            var js = host.Services.GetRequiredService<IJSRuntime>();
+            string storedCulture = await js.InvokeAsync<string>("localStorage.getItem", "blazorCulture");
+
+            CultureInfo culture;
+            if (!string.IsNullOrWhiteSpace(storedCulture))
+            {
+                culture = new CultureInfo(storedCulture);
+            }
+            else
+            {
+                // fallback to browser language, or "en" if not supported
+                var browserLang = await js.InvokeAsync<string>("navigator.language");
+                culture = new CultureInfo(browserLang ?? "en");
+            }
+
+            // 3) Apply culture globally
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+            // 4) Run
+            await host.RunAsync();
+        }
+    }
 }
-await host.RunAsync();
